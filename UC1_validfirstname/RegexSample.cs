b902e4aa154ba7b5be6d3c56882247333fb85dc7
ovi2410 <guneshwarifegade@gmail.com>
2022-05-24 21:45:34 +0530

    valid first name

---
+++ UC1_validfirstname/RegexSample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC1_validfirstname
+{
+    class RegexSample
+    {
+        string pattern = "^[A-Za-z]{3,}$";
+        public void Validating()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Validating The first Name");
+            ValidatingfirstName();
+
+        }
+        public void ValidatingfirstName()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Enter the first name : ");
+            string input1 = Console.ReadLine();
+            bool res1 = regex.IsMatch(input1);
+            if (res1)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
+
