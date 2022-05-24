3de92b76dde348fc7b86dd2f5cd62e8e6bcc964c
ovi2410 <guneshwarifegade@gmail.com>
2022-05-24 22:02:27 +0530

    atleast one upper case

--- UC6_AtleastOneUpperCase/Program.cs
+++ UC6_AtleastOneUpperCase/Program.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UC5_validPassword;
+using UC6_AtleastOneUpperCase;
 
 namespace UserRegisteration
 {
--- UC6_AtleastOneUpperCase/RegexSample.cs
+++ UC6_AtleastOneUpperCase/RegexSample.cs
@@ -5,21 +5,22 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace UC5_validPassword
+namespace UC6_AtleastOneUpperCase
 {
     class RegexSample
     {
         public void Validating()
         {
-            Console.WriteLine("--------------------------");
             Console.WriteLine("Validating password");
             ValidatingPassWord();
+            Console.WriteLine("--------------------------");
         }
         public void ValidatingPassWord()
         {
-            string passwordPattern = @"[a-z,A-Z,0-9]{8,}$";
+            //string passwordPattern = @"[a-z,A-Z,0-9]{8,}$";
+            string passwordPattern = @"^(?=.*[A-Z]).{8,}$";
             Regex regex = new Regex(passwordPattern);
-            Console.WriteLine("Enter password minimum 8 characters");
+            Console.WriteLine("Enter password minimum 8 characters with one upper case");
             string password = Console.ReadLine();
             bool res = regex.IsMatch(password);
             if (res)
@@ -30,9 +31,8 @@ namespace UC5_validPassword
             {
                 Console.WriteLine("invalid password");
             }
+
         }
+
     }
 }
-
-
-        
